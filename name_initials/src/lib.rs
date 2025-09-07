7c3c7185cd1b80257222060e3e04d56f4fb0b585
asaadane <ayman.saadane@gmail.com>
2025-09-07 02:17:37 +0100

    add name_initials

---
+++ name_initials/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn initials(names: Vec<&str>) -> Vec<String> {
+    names
+        .iter()
+        .map(|name| {
+            let mut parts = name.splitn(2, ' ');
+            let first = parts.next().expect("wrong format")[0..1].to_string() + ".";
+            let last = parts.next().expect("wrong format")[0..1].to_string() + ".";
+            first + " " + &last
+        })
+        .collect::<Vec<String>>()
+}
